74c3c42f17688205cf1728ab37ed90a2e530c089
Mat Johnson <7blickoff7@gmail.com>
2024-05-03 23:02:19 +0300

    Initial commit

---
+++ src/test/UnitTestProject1/FacadeTest.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Facade.Conceptual;  
+
+namespace Tests
+{
+    [TestClass]
+    public class FacadeTests
+    {
+        [TestMethod]
+        public void TestFacadeOperation()
+        {
+            // Організація
+            var subsystem1 = new Subsystem1();
+            var subsystem2 = new Subsystem2();
+            var facade = new Facade.Conceptual.Facade(subsystem1, subsystem2);
+
+            // Очікуване значення
+            var expected = "Facade initializes subsystems:\n" +
+                           "Subsystem1: Ready!\n" +
+                           "Subsystem2: Get ready!\n" +
+                           "Facade orders subsystems to perform the action:\n" +
+                           "Subsystem1: Go!\n" +
+                           "Subsystem2: Fire!\n";
+
+            // Виконання
+            var result = facade.Operation();
+
+            // Перевірка
+            Assert.AreEqual(expected, result);  // Перевіряємо, чи правильний результат
+        }
+    }
+}
