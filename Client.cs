31cab901a12b932c092bc4e34793a88e2fca3b2a
Mat Johnson <7blickoff7@gmail.com>
2024-05-03 21:41:51 +0300

    first commit

---
+++ Client.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Facade.Conceptual
+{
+
+
+    class Client
+    {
+        public static void ClientCode(Facade facade)
+        {
+            Console.Write(facade.Operation());
+        }
+    }
+}   
